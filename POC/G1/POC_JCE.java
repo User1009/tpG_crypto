40971b3964c623a070557f07fcf39e5a3fb8b39b
etienne <a>
2020-04-10 16:26:25 +0200

    Début Tp sans JCE

--- POC/G1/POC_JCE.java
+++ POC/G1/POC_JCE.java
@@ -1,13 +1,13 @@
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
 import java.math.BigInteger;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPublicKeySpec;
-import java.util.Arrays;
 
-public class POC {
+public class POC_JCE {
 
     private BigInteger n = new BigInteger("94f28651e58a75781cfe69900174b86f855f092f09e3da2ad86b4ed964a84917e5ec60f4ee6e3adaa13962884e5cf8dae2e0d29c6168042ec9024ea11176a4ef031ac0f414918b7d13513ca1110ed80bd2532f8a7aab0314bf54fcaf621eda74263faf2a5921ffc515097a3c556bf86f2048a3c159fccfee6d916d38f7f23f21",16);
     private BigInteger e = new BigInteger("44bb1ff6c2b674798e09075609b7883497ae2e2d7b06861ef9850e26d1456280523319021062c8743544877923fe65f85111792a98e4b887de8ffd13aef18ff7f6f736c821cfdad98af051e7caaa575d30b54ed9a6ee901bb0ffc17e25d444f8bfc5922325ee2ef94bd4ee15bede2ea12eb623ad507d6b246a1f0c3cc419f155",16);
@@ -16,11 +16,9 @@ public class POC {
     private IvParameterSpec iv;
     private PublicKey publicKey_rsa;
 
-    private byte[] chiffre;
 
     private void gen_aes_key () {
         KeyGenerator keyGen;
-        SecretKey secretKey=null;
 
         try {
             keyGen = KeyGenerator.getInstance("AES");
@@ -53,30 +51,52 @@ public class POC {
         try {
             Cipher chiffreur = Cipher.getInstance("RSA/ECB/PKCS1Padding");
             chiffreur.init(Cipher.ENCRYPT_MODE,publicKey_rsa);
-            //chiffre = chiffreur.doFinal(k_aes.getEncoded());
 
-            FileOutputStream outputStream = new FileOutputStream("resultat.txt");
-            //System.out.println("message encrypté : 0x" + chiffre.length + Arrays.toString(chiffre));
+            FileOutputStream outputStream = new FileOutputStream("POC/G1/resultat.txt");
             CipherOutputStream cos = new CipherOutputStream(outputStream,chiffreur);
+
             cos.write(k_aes.getEncoded());
             cos.write(iv.getIV());
             cos.close();
 
-            /*final PrintWriter pw = new PrintWriter(cos);
-            pw.flush();
-            cos.write(iv.getIV());
-            pw.close();*/
         } catch (Exception e1) {
             e1.printStackTrace();
         }
     }
 
+    private void chiffrer(String path){
+
+        File f = new File(path);
+        try {
+            FileInputStream fis= new FileInputStream(f);
+            FileOutputStream fos = new FileOutputStream("POC/G1/resultat.txt");
+
+            Key aesKey = new SecretKeySpec(k_aes.getEncoded(),"AES");
+
+            Cipher chiffreur = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            chiffreur.init(Cipher.ENCRYPT_MODE, aesKey);
+
+            CipherOutputStream cos = new CipherOutputStream(fos, chiffreur);
+            int read;
+            byte[] buf = new byte[1024];
+            while((read = fis.read(buf)) != -1) {
+                cos.write(buf, 0, read);
+            }
+            cos.close();
+
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] args) {
-        POC poc = new POC();
+        POC_JCE poc = new POC_JCE();
         poc.gen_aes_key();
         poc.gen_iv();
         poc.gen_rsa_key();
         poc.rsa_encrypt();
+        poc.chiffrer("butokuden.jpg");
     }
 
 }
